5a0c4302b8156931380fd31188197cfdf2bdf5ff
Sahil Mahadik <mahadiksahil60@gmail.com>
2024-06-17 16:46:06 +0530

    ejs added

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ import express from 'express'
 import urlroute from './routes/url.js';
 import connectMongoDb from './connect.js';
 import URL from './models/url.js'
-
+import path from 'path'
 const app = express();
 const PORT = 4000; 
 const DATABASE_NAME = "short-url"
 
 connectMongoDb(`mongodb+srv://mahadiksahil60:sahil123@cluster0.9ikbkik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${DATABASE_NAME}`)
 .then(()=>{console.log("Database connected successfully")})
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
 app.use("/url", urlroute);
-app.get('/:shortID', async ( req, res)=>{
+app.get('/url/:shortID', async ( req, res)=>{
     const shortID = req.params.shortID;
     const entry = await URL.findOneAndUpdate({
         shortID,
@@ -26,6 +28,13 @@ app.get('/:shortID', async ( req, res)=>{
     res.redirect(entry.redirectUrl)
 })
 
+app.get("/test", async (req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render('home', {
+        urls: allUrls,
+
+    });
+})
 
 
 app.listen(PORT, ()=>{
